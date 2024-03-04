c291da4b9355265cd5ae6fd4bbfc2d07efe9f3ee
merlina-wizard <marti@LAPTOP-31322K6I.localdomain>
2024-03-04 14:16:58 +0100

    primo

---
+++ so_long/so_long.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mamerlin <mamerlin@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 13:02:23 by mamerlin          #+#    #+#             */
+/*   Updated: 2024/03/04 13:02:24 by mamerlin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "ft_printf/ft_printf.h"
+# include "get_next_line/get_next_line.h"
+
+
+#endif
